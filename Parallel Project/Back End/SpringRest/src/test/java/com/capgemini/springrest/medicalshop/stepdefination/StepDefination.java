1101c0db7dab36732e2da7340688a94e45e89699
akash hadal <akash.hadal@gmail.com>
2019-12-16 00:03:59 +0530

    1.2

--- Parallel Project/Back End/SpringRest/src/test/java/com/capgemini/springrest/medicalshop/stepdefination/StepDefination.java
+++ Parallel Project/Back End/SpringRest/src/test/java/com/capgemini/springrest/medicalshop/stepdefination/StepDefination.java
@@ -22,6 +22,7 @@ public class StepDefination {
 	public void the_user_has_loaded_the_application_in_the_browser() throws Throwable {
 		driver=new ChromeDriver();
 		driver.get("http://localhost:4200/");
+		driver.manage().window().maximize();
 		//Thread.sleep(2000);
 	}
 	
@@ -78,7 +79,6 @@ public class StepDefination {
 
 	@When("^the user selects the  role in the role dropdown$")
 	public void the_user_selects_the_role_in_the_role_dropdown() throws Throwable {
-		//driver.findElement(By.name("userEmail")).sendKeys("pooja@gmail.com");
 		WebElement listBox= driver.findElement(By.name("role"));
 		Select select=new Select(listBox);
 		select.selectByVisibleText("User");
@@ -88,6 +88,87 @@ public class StepDefination {
 	public void the_user_clicks_on_the_register_Button() throws Throwable {
 		driver.findElement(By.xpath("//button[text()='Register']")).click();
 		Thread.sleep(10000);
+	}
+	
+	@When("^the user clicks on product list on navbar$")
+	public void the_user_clicks_on_product_list_on_navbar() throws Throwable {
+		driver.findElement(By.xpath("//a[text()='Product List']")).click();
+		Thread.sleep(1000);
+	}
+
+	@When("^the user clicks on add button$")
+	public void the_user_clicks_on_add_button() throws Throwable {
+		driver.findElement(By.xpath("//button[text()=' Add ']")).click();
+		Thread.sleep(1000);
+	}
+
+	@When("^the user clicks on cart button on navbar$")
+	public void the_user_clicks_on_cart_button_on_navbar() throws Throwable {
+		driver.findElement(By.xpath("//a[text()='Cart']")).click();	 
+		Thread.sleep(1000);
+	}
+
+	@When("^the user clicks on payment button$")
+	public void the_user_clicks_on_payment_button() throws Throwable {
+		driver.findElement(By.xpath("//a[text()='Payment']")).click();
+	}
+
+	@Then("^the payment page must be loaded$")
+	public void the_payment_page_must_be_loaded() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		//String expected="actiTIME - Enter Time-Track";
+		//String actual= driver.getTitle();
+		//Assert.assertEquals(actual, expected);
+	}
+
+	@When("^the user enters the name in the card Holder's Name textbox$")
+	public void the_user_enters_the_name_in_the_card_Holder_s_Name_textbox() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		driver.findElement(By.name("userName")).sendKeys("Akash Ramesh Hadal");
+	}
+
+	@When("^the user enters the card number in card nubmer textbox$")
+	public void the_user_enters_the_card_number_in_card_nubmer_textbox() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		driver.findElement(By.name("userCard")).sendKeys("5421658474512365");
+	}
+
+	@When("^the user selects the expiry date in the month dropdown of card expiry date$")
+	public void the_user_selects_the_expiry_date_in_the_month_dropdown_of_card_expiry_date() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		WebElement listBox= driver.findElement(By.name("userCardExpiry"));
+		Select select=new Select(listBox);
+		select.selectByVisibleText("February");
+	}
+
+	@When("^the user enters the year in the year textbox of card expiry date$")
+	public void the_user_enters_the_year_in_the_year_textbox_of_card_expiry_date() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		driver.findElement(By.id("year")).sendKeys("2024");
+	}
+
+	@When("^the user enters the cvv in the cvv textbox$")
+	public void the_user_enters_the_cvv_in_the_cvv_textbox() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		driver.findElement(By.name("cvv")).sendKeys("123");
+	}
+
+	@When("^the user enters the address in the address textbox$")
+	public void the_user_enters_the_address_in_the_address_textbox() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		driver.findElement(By.name("address")).sendKeys("Boisar");
+		Thread.sleep(2000);
+	}
+
+	@When("^the user clicks on the submit button$")
+	public void the_user_clicks_on_the_submit_button() throws Throwable {
+		driver.findElement(By.xpath("//button[text()='Submit']")).click();	
+		Thread.sleep(2000);
+	}
+
+	@Then("^the order history page must be loaded$")
+	public void the_order_history_page_must_be_loaded() throws Throwable {
+		driver.close();
 	}
 	
 }
